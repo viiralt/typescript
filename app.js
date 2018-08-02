d7d8d6c339fd435c96de7e70403a792642bfd593
Viiralt <viiralt.h@gmail.com>
2018-08-02 13:31:18 +0300

    type: tuples

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+"use strict";
 var Greeter = /** @class */ (function () {
     function Greeter(message) {
         this.greeting = message;
---
+++ app.ts
@@ -0,0 +1,27 @@
+// string
+let myName = 'Henri';
+myName = 28;
+
+// number
+let myNumber = 24;
+myNumber = 'Henri';
+
+// boolean
+let hasHobbies: string[];
+hasHobbies = 1;
+
+// assign types
+let myRealAge: number;
+myRealAge = '23';
+
+// arrays
+hasHobbies = ['cooking', 'fitness', 'photography'];
+hasHobbies[1]; //?
+typeof hasHobbies; //?
+
+let hobbies: any[] = [123, 'boom', true];
+hobbies; //?
+
+// tuples - mixed, sequential type
+let address: [string, number] = ['somestreet', 132 - 22];
+address = [132 - 22, 'bububu'];
